6874d77c573db4e8cc8817caf0b4fce7ab47d231
Brian Dukes <bdukes@engagesoftware.com>
2020-07-20 14:57:27 -0500

    Add support for --installation-status-timeout

--- cantarus-polydeploy-client/index.js
+++ cantarus-polydeploy-client/index.js
@@ -5,13 +5,16 @@ const deployPath = taskLib.getPathInput("deploy-path", true, true);
 const targetUri = taskLib.getInput("target-uri", true);
 const apiKey = taskLib.getInput("api-key", true);
 const encryptionKey = taskLib.getInput("encryption-key", true);
+const installationStatusTimeout = taskLib.getInput(
+  "installation-status-timeout"
+);
 
-taskLib.findMatch(deployPath, "**/*.zip").forEach(zipPath => {
+taskLib.findMatch(deployPath, "**/*.zip").forEach((zipPath) => {
   taskLib.debug(`Copying ${zipPath} to ${taskLib.cwd()}`);
   taskLib.cp(zipPath, taskLib.cwd());
 });
 
-const result = taskLib
+let toolRunner = taskLib
   .tool(path.join(__dirname, "DeployClient.exe"))
   .arg("--target-uri")
   .arg(targetUri)
@@ -19,8 +22,15 @@ const result = taskLib
   .arg(apiKey)
   .arg("--encryption-key")
   .arg(encryptionKey)
-  .arg("--no-prompt")
-  .execSync();
+  .arg("--no-prompt");
+
+if (installationStatusTimeout) {
+  toolRunner = toolRunner
+    .arg("--installation-status-timeout")
+    .arg(installationStatusTimeout);
+}
+
+const result = toolRunner.execSync();
 
 if (result.code != 0) {
   throw new Error(result.error ? result.error.message : result.stderr);
